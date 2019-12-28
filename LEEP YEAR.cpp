b2e6f24db007356cb9ae6e6ccbe52548fb2cd5be
mainulislamik <59301781+mainulislamik@users.noreply.github.com>
2019-12-28 11:52:07 +0600

    Update LEEP YEAR.cpp

--- LEEP YEAR.cpp
+++ LEEP YEAR.cpp
@@ -51,3 +51,40 @@ int main()
     cout<<"\n\nname: "<<kamal.name <<"\nid: "<<kamal.id <<"\ngpa: " <<kamal.gpa;
     getch();
 }
+//c++ class useing function+perameeter
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class student
+{
+    public:
+        string name;
+
+        int id;
+        double gpa;
+        void display()
+        {
+            cout<<"name: "<<name <<"\nid: "<<id <<"\ngpa: " <<gpa<<"\n\n";
+        }
+        void setvalu( string m,int y, double z)
+        {
+
+            name=m;
+            id=y;
+            gpa=z;
+        }
+};
+
+int main()
+{
+    student nahid;
+    nahid.setvalu("nahid",1001,4.20);
+    nahid.display();
+
+    student kamal;
+    kamal.setvalu("kamal",1002,4.22);
+    kamal.display();
+
+    getch();
+}
